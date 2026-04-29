b8cbf72e977e46b9e79b15f504febd3f3b6defd8
William Rodrigues <wllir12@hotmail.com>
2026-04-29 20:15:17 -0300

    Melhorando contraste, tamanho dos projetos e navegação dinâmica

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-about',
+  standalone: true,
   imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
+  @Output() navigate = new EventEmitter<string>();
 
+  onContactClick() {
+    this.navigate.emit('contact');
+  }
+
+  onProjectsClick() {
+    this.navigate.emit('projects');
+  }
 }
